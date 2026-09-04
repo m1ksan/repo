49f6ac5140b6a200396b5d217372ba4056d4bbb4
nasi pecel <101936661+m1ksan@users.noreply.github.com>
2026-09-04 08:41:59 +0700

    update 849

--- macSP849/849.sh
+++ macSP849/849.sh
@@ -1,74 +1,77 @@
 bq query --use_legacy_sql=false "$(cat <<'SQL'
 SELECT
- date,
- label,
- (team1.score + team2.score) AS totalGoals
+  date,
+  label,
+  (team1.score + team2.score) AS totalGoals
 FROM
- `soccer.matches` Matches
+  `soccer.matches` Matches
 LEFT JOIN
- `soccer.competitions` Competitions ON
-   Matches.competitionId = Competitions.wyId
+  `soccer.competitions` Competitions ON
+    Matches.competitionId = Competitions.wyId
 WHERE
- status = 'Played' AND
- Competitions.name = 'Spanish first division'
+  status = 'Played' AND
+  Competitions.name = 'Spanish first division'
 ORDER BY
- totalGoals DESC, date DESC
+  totalGoals DESC, date DESC
+SQL
 )"
 
 bq query --use_legacy_sql=false "$(cat <<'SQL'
 SELECT
- playerId,
- (Players.firstName || ' ' || Players.lastName) AS playerName,
- COUNT(id) AS numPasses
+  playerId,
+  (Players.firstName || ' ' || Players.lastName) AS playerName,
+  COUNT(id) AS numPasses
 
 FROM
- `soccer.events` Events
+  `soccer.events` Events
 
 LEFT JOIN
- `soccer.players` Players ON
-   Events.playerId = Players.wyId
+  `soccer.players` Players ON
+    Events.playerId = Players.wyId
 
 WHERE
- eventName = 'Pass'
+  eventName = 'Pass'
 
 GROUP BY
- playerId, playerName
+  playerId, playerName
 
 ORDER BY
- numPasses DESC
+  numPasses DESC
 
 LIMIT 10
+SQL
 )"
 
 bq query --use_legacy_sql=false "$(cat <<'SQL'
 SELECT
- playerId,
- (Players.firstName || ' ' || Players.lastName) AS playerName,
- COUNT(id) AS numPKAtt,
- SUM(IF(101 IN UNNEST(tags.id), 1, 0)) AS numPKGoals,
+  playerId,
+  (Players.firstName || ' ' || Players.lastName) AS playerName,
+  COUNT(id) AS numPKAtt,
+  SUM(IF(101 IN UNNEST(tags.id), 1, 0)) AS numPKGoals,
 
- SAFE_DIVIDE(
-   SUM(IF(101 IN UNNEST(tags.id), 1, 0)),
-   COUNT(id)
-   ) AS PKSuccessRate
+  SAFE_DIVIDE(
+    SUM(IF(101 IN UNNEST(tags.id), 1, 0)),
+    COUNT(id)
+  ) AS PKSuccessRate
 
 FROM
- `soccer.events` Events
+  `soccer.events` Events
 
 LEFT JOIN
- `soccer.players` Players ON
-   Events.playerId = Players.wyId
+  `soccer.players` Players ON
+    Events.playerId = Players.wyId
 
 WHERE
- eventName = 'Free Kick' AND
- subEventName = 'Penalty'
+  eventName = 'Free Kick' AND
+  subEventName = 'Penalty'
 
 GROUP BY
- playerId, playerName
+  playerId, playerName
 
 HAVING
- numPkAtt >= 5
+  numPKAtt >= 5
 
 ORDER BY
- PKSuccessRate DESC, numPKAtt DESC
+  PKSuccessRate DESC, numPKAtt DESC
+SQL
 )"
